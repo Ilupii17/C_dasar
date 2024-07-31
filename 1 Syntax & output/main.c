efd6088cdad373c72962db5808a3b70bf796a69a
Muhammad Arham <154342230+Reinnaa@users.noreply.github.com>
2024-07-31 10:18:39 +0800

    Add files via upload

---
+++ 1 Syntax & output/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("hallo dunia\nHello world"); // gunakan (\n) newline	dan (\t) untuk tab
+	printf("\tHello mom");				// dan (\") untuk single quote
+	return 0;
+}
+
+/* ini adalah multi line comments 
+   ahahhaahha 
+
+   */
